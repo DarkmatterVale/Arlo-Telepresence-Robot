169f5e4bb29962ce3bc472e2627c16bcef6d54e0
Vale <valetolpegin@gmail.com>
2014-09-28 09:12:12 -0400

    Update SimpleIDECode.c

--- Basic-Communication/SimpleIDECode.c
+++ Basic-Communication/SimpleIDECode.c
@@ -1,1 +1,45 @@
-//Add SimpleIDE code for communication with a JAVA program on a Windows OS here
+/*
+
+Base code courtesy of ChrisL8, edited by DarkmatterVale
+
+*/
+
+#include "simpletools.h"
+#include "fdserial.h"
+
+fdserial *term;
+
+int main()
+{
+  simpleterm_close(); // Close simplex serial terminal
+  term = fdserial_open(31, 30, 0, 115200); // Open Full Duplex serial connection
+
+  while(1)
+  {
+    dprint(term, "Enter a string of 50 characters or less:\n");
+    char buf[51]; // A Buffer long enough to hold the longest line you may send.
+    int count = 0;
+    while (count < 51) {
+      buf[count] = fdserial_rxChar(term);
+      if (buf[count] == '\r' || buf[count] == '\n')
+        break;
+        
+      if ( buf[count] == '1' )
+      {
+        high( 26 );
+        low( 27 );
+      }
+      
+      if ( buf[count] == '2' )
+      {
+        low( 26 );
+        high( 27 );
+      }        
+        
+      count++;
+    }
+    buf[count] = '\0';
+        
+    dprint(term, "%s\n", buf);
+  }
+}
