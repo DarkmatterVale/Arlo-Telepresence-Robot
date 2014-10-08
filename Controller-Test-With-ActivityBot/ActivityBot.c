0c54d6c4510c38f53d5a526b06a68533bb321eb8
Vale <valetolpegin@gmail.com>
2014-10-08 11:50:24 -0400

    added pseudocode

--- Controller-Test-With-ActivityBot/ActivityBot.c
+++ Controller-Test-With-ActivityBot/ActivityBot.c
@@ -1,1 +1,14 @@
 //Add ActivityBot code here for moving the ActivityBot based on the value returned from the controller
+
+
+//PSEUDOCODE:
+//while true
+  //Read in computer signals
+  //if forward
+    //go forward
+  //else if left
+    //turn left
+  //else if right
+    //turn right
+  //else if backward
+    //go backward
