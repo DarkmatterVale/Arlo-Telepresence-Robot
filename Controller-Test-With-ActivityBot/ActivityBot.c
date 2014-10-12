95ce0623b7db9624ef806ae376cde60329ac7eb8
Vale <valetolpegin@gmail.com>
2014-10-12 09:04:25 -0400

    added code V1

--- Controller-Test-With-ActivityBot/ActivityBot.c
+++ Controller-Test-With-ActivityBot/ActivityBot.c
@@ -1,5 +1,4 @@
-//Add ActivityBot code here for moving the ActivityBot based on the value returned from the controller
-
+//ActivityBot code here for moving the ActivityBot based on the value returned from the controller
 
 //PSEUDOCODE:
 //while true
@@ -12,3 +11,38 @@
     //turn right
   //else if backward
     //go backward
+
+#include "simpletools.h"                      // Include simple tools
+#include "fdserial.h"
+#include "abdrive.h"
+
+fdserial *xbee;
+
+int main()                                    // Main function
+{
+  xbee = fdserial_open( 9, 8, 0, 9600 );
+
+  char c;
+
+  while ( 1 )
+  {
+    c = fdserial_rxChar( xbee );
+ 
+    if ( c == 'f' )
+    {
+      drive_speed( 64, 64 );
+    } else if ( c == 'b' )
+    {
+      drive_speed( -64, -64 );
+    } else if ( c == 'l' )
+    {
+      drive_speed( 0, 64 );
+    } else if ( c == 'r' )
+    {
+      drive_speed( 64, 0 );
+    } else if ( c == 's' )
+    {
+      drive_speed( 0, 0 );
+    }
+  }
+}
