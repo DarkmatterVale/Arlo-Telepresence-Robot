de179d8169b4d8f262fdc5209040fa02f627ddda
Vale <valetolpegin@gmail.com>
2014-10-16 07:56:28 -0400

    Added Gmail code to comments

--- Gmail-ActivityBot-Remote-Control/JavaOutput.java
+++ Gmail-ActivityBot-Remote-Control/JavaOutput.java
@@ -8,6 +8,53 @@ To Do:
   -Add Gmail functionality
   -Remove SerialPortReader
 
+Implement Gmail Code:
+        String host = "smtp.gmail.com";
+        String username = "*****************@gmail.com";
+        String password = "*************";
+        Properties props = new Properties();
+        // set any needed mail.smtps.* properties here
+        Session session = Session.getInstance(props);
+        MimeMessage msg = new MimeMessage(session);
+        // set the message content here
+        try
+        {
+            msg.setFrom();      
+            msg.setRecipients(Message.RecipientType.TO, "************@gmail.com" );
+            msg.setSubject("Subject");
+            msg.setContent("Brought to you by Carls Junior", "text/html;charset=UTF-8"); 
+            Transport t = session.getTransport("smtps");
+            try {
+                t.connect(host, username, password);
+                t.sendMessage(msg, msg.getAllRecipients());
+            } catch ( Exception ex )
+            {
+                ex.printStackTrace();
+            } finally {
+                t.close();
+            }
+        } catch ( Exception ex )
+        {
+            ex.printStackTrace();
+        }
+        
+        try
+        {
+            Store store = session.getStore("imaps");
+            store.connect("imap.gmail.com", "**********@gmail.com", "***********");
+            System.out.println(store);
+
+            Folder inbox = store.getFolder("Inbox");
+            inbox.open(Folder.READ_ONLY);
+            Message messages[] = inbox.getMessages();
+            for(Message message : messages) {
+                System.out.println( message.getSubject() );
+            }
+        } catch ( Exception ex )
+        {
+            ex.printStackTrace();
+        }
+        
 */
 
 /**
