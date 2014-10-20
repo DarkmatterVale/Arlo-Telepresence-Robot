dd51e42f06d2cdc2dbec58efb7f1f16775677691
Vale <valetolpegin@gmail.com>
2014-10-20 14:07:05 -0400

    Removed unneeded Gmail code

--- Gmail-ActivityBot-Remote-Control/JavaOutput.java
+++ Gmail-ActivityBot-Remote-Control/JavaOutput.java
@@ -9,35 +9,6 @@ To Do:
   -Remove SerialPortReader
 
 Implement Gmail Code:
-        String host = "smtp.gmail.com";
-        String username = "*****************@gmail.com";
-        String password = "*************";
-        Properties props = new Properties();
-        // set any needed mail.smtps.* properties here
-        Session session = Session.getInstance(props);
-        MimeMessage msg = new MimeMessage(session);
-        // set the message content here
-        try
-        {
-            msg.setFrom();      
-            msg.setRecipients(Message.RecipientType.TO, "************@gmail.com" );
-            msg.setSubject("Subject");
-            msg.setContent("Brought to you by Carls Junior", "text/html;charset=UTF-8"); 
-            Transport t = session.getTransport("smtps");
-            try {
-                t.connect(host, username, password);
-                t.sendMessage(msg, msg.getAllRecipients());
-            } catch ( Exception ex )
-            {
-                ex.printStackTrace();
-            } finally {
-                t.close();
-            }
-        } catch ( Exception ex )
-        {
-            ex.printStackTrace();
-        }
-        
         try
         {
             Store store = session.getStore("imaps");
