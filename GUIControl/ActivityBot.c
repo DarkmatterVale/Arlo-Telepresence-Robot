d2e28f9bf54ade1a9c33c563672621141a4576f0
Vale <valetolpegin@gmail.com>
2014-10-25 17:23:17 -0400

    Added code

--- GUIControl/ActivityBot.c
+++ GUIControl/ActivityBot.c
@@ -1,1 +1,42 @@
-Code for ActivityBot goes here
+//Code for the ActivityBot robot
+
+/*
+
+Author: DarkmatterVale
+
+*/
+
+#include "simpletools.h"                      // Include simple tools
+#include "fdserial.h"
+#include "abdrive.h"
+
+fdserial *xbee; //Create full-duplex XBee serial communication variable
+
+int main()                                    // Main function
+{
+  xbee = fdserial_open( 9, 8, 0, 9600 ); //Initialize communication
+
+  char c; //Create char
+
+  while ( 1 )
+  {
+    c = fdserial_rxChar( xbee ); //get all values sent to the ActivityBot
+ 
+    if ( c == 'f' ) //if the robot was told to go forward, go forward
+    {
+      drive_speed( 64, 64 );
+    } else if ( c == 'b' ) //if the robot was told to go backward, go backward
+    {
+      drive_speed( -64, -64 );
+    } else if ( c == 'l' ) //if the robot was told to go left, go left
+    {
+      drive_speed( 0, 64 );
+    } else if ( c == 'r' ) //if the robot was told to go right, go right
+    {
+      drive_speed( 64, 0 );
+    } else if ( c == 's' ) //if the robot is told to stop, stop
+    {
+      drive_speed( 0, 0 );
+    }
+  }
+}
