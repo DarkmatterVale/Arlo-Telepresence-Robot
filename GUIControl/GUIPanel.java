65c126784e455b8be66f6bcf613e98e3449b8e97
Vale <valetolpegin@gmail.com>
2014-10-26 11:53:29 -0400

    Added comments

--- GUIControl/GUIPanel.java
+++ GUIControl/GUIPanel.java
@@ -1,1 +1,29 @@
-Code for Panel that will hold all of the UI
+/*
+
+Author: DarkmatterVale
+
+*/
+
+/*
+
+To Do list:
+  -Add basic GUI
+
+*/
+
+/*
+
+Design:
+
+For the first version ( v1.0 ) of the GUI, I am thinking of a BorderLayout style with different areas seperated by borders.
+The first version shall have 1 non-editable text field that will display the kind of input allowed. In addition, the first
+version should also include an exit button.
+
+ALL OTHER FEATURES SHOULD BE ADDED TO LATER VERSIONS
+
+*/
+
+public class GUIPanel
+{
+  //Code for GUI goes in here
+}
