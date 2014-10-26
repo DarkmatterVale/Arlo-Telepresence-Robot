add1fd873e48f1f543e60edfd96c8ee4d09a3c4c
Vale <valetolpegin@gmail.com>
2014-10-26 09:23:10 -0400

    Added v1.0 code

--- GUIControl/Input.java
+++ GUIControl/Input.java
@@ -1,1 +1,33 @@
 Code for decidng which input to use ( arrow or controller )
+
+public class Input
+{
+  public Input()
+  {
+  }
+  
+  public int getInput( int decision )
+  {
+    //If the passed value is 1, get input from arrow keys
+    //Else, get input from the controller
+    //Then return the value
+    
+    switch decision:
+      case 0:
+        return controllerInput();
+      case 1:
+        return arrowKeyInput();
+    
+    return -1;
+  }
+  
+  public String controllerInput()
+  {
+    //In controllerInput, get values from Joystick on ActivityBoard
+  }
+  
+  public String arrowKeyInput()
+  {
+    //In arrowKeyInput, get values from arrow keys on computer
+  }
+}
