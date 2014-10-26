f058cc85d8351a839e76ec84634080e2cf045209
Vale <valetolpegin@gmail.com>
2014-10-26 09:50:26 -0400

    Update and rename InputMethod.java to ArrowKeyInput.java

---
+++ GUIControl/ArrowKeyInput.java
@@ -0,0 +1,17 @@
+public class ArrowKeyInput
+{
+  public ArrowKeyInput()
+  {
+  }
+  
+  public String arrowKeyInput()
+  {
+    //In arrowKeyInput, get values from arrow keys on computer
+    //Send email which arrow key is pressed ( sendMail() )
+  }
+  
+  public void sendMail()
+  {
+    //Add Gmail sending component here
+  }
+}
