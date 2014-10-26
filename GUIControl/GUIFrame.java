c6207b71369c1e1f06a852dcc16bae6cd29339c9
Vale <valetolpegin@gmail.com>
2014-10-26 15:17:49 -0400

    Added frame setup code

--- GUIControl/GUIFrame.java
+++ GUIControl/GUIFrame.java
@@ -10,17 +10,37 @@ To Do list:
   - Add setter and getter methods
 */
 
-public class GUIFrame
+import javax.swing.*;
+import java.awt.*;
+
+public class GUIFrame extends JFrame
 {
+  JFrame missionControl;
+  
   public GUIFrame()
   {
-    //Call setupFrame()
+    //Initialize the JFrame
+    missionControl = new JFrame();
+    
+    //Call setupFrame() to set up the frame
+    setupFrame();
+    
     //set frame to visible
+    missionControl.setVisible( true );
   }
   
   public setupFrame()
   {
+    //Set frame settings
+    missionControl.setTitle( "Arlo Telepresence Device Mission Control" );
+    missionControl.setDefaultCloseOption( JFrame.EXIT_ON_CLOSE );
+    missionControl.setSize( 500, 500 );
+    
     //Create a GUIPanel object
+    missionControlPanel = new GUIPanel();
+    
+    //Add panel to frame
+    missionControl.add( missionControlPanel );
   }
   
   //Add setter and getter methods below
