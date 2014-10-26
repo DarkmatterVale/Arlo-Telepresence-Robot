7763328d612a5fec8869f120273d0261043956f7
Vale <valetolpegin@gmail.com>
2014-10-26 13:43:01 -0400

    Added comments for what will be in v1.0

--- GUIControl/GUIPanel.java
+++ GUIControl/GUIPanel.java
@@ -15,10 +15,11 @@ To Do list:
 
 Design:
 
-For the first version ( v1.0 ) of the GUI, I am thinking of a BorderLayout style with different areas seperated by borders.
-The first version shall have 1 non-editable text field that will display the kind of input allowed. In addition, the first
-version should also include an exit button, start and stop buttons, and an "LED" that will be green if the session/communication
-is working and red if the session/communication with the robot is not working.
+For the first version ( v1.0 ) of the GUI, I am thinking of a BorderLayout style with different areas [eventually]
+seperated by borders. The first version shall have 1 non-editable text field that will display what is being sent. In 
+addition, the first version should also include an exit button, start and stop buttons, and an "LED" that will be green if 
+the session/communication is working and red if the session/communication with the robot is not working. Finally, the 1st
+version shall include a Gmail status indicator which will be a JLabel that is in one of two states, OK or NOT WORKING.
 
 ALL OTHER FEATURES SHOULD BE ADDED TO LATER VERSIONS
 
