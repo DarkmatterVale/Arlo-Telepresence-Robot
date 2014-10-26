23f834468267013eaf0049683832b745d4660f93
Vale <valetolpegin@gmail.com>
2014-10-26 12:08:56 -0400

    Update GUIPanel.java

--- GUIControl/GUIPanel.java
+++ GUIControl/GUIPanel.java
@@ -17,7 +17,8 @@ Design:
 
 For the first version ( v1.0 ) of the GUI, I am thinking of a BorderLayout style with different areas seperated by borders.
 The first version shall have 1 non-editable text field that will display the kind of input allowed. In addition, the first
-version should also include an exit button.
+version should also include an exit button, start and stop buttons, and an "LED" that will be green if the session/communication
+is working and red if the session/communication with the robot is not working.
 
 ALL OTHER FEATURES SHOULD BE ADDED TO LATER VERSIONS
 
