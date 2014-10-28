5b720dfa68362e00b4cddd4c57592bc7231a508a
Vale <valetolpegin@gmail.com>
2014-10-28 17:55:17 -0400

    Added v0.2 code

--- GUIControl/GUIFrame.java
+++ GUIControl/GUIFrame.java
@@ -15,33 +15,26 @@ import java.awt.*;
 
 public class GUIFrame extends JFrame
 {
-  //Create the JFrame and JPanel to hold all of the content
-  JFrame missionControl;
-  JPanel missionControlPanel;
-  
   public static void main( String [] args )
   {
     //Initialize the JFrame
-    missionControl = new JFrame();
-    
-    //Call setupFrame() to set up the frame
-    setupFrame();
+    JFrame missionControl = new GUIFrame();
     
     //set frame to visible
     missionControl.setVisible( true );
   }
   
-  public void setupFrame()
+  public void GUIFrame()
   {
     //Set frame settings
-    missionControl.setTitle( "Arlo Telepresence Device Mission Control" );
-    missionControl.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-    missionControl.setSize( 500, 500 );
+    setTitle( "Arlo Telepresence Device Mission Control" );
+    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    setSize( 500, 500 );
     
     //Create a GUIPanel object
-    missionControlPanel = new GUIPanel();
+    JPanel missionControlPanel = new GUIPanel();
     
     //Add panel to frame
-    missionControl.add( missionControlPanel );
+    add( missionControlPanel );
   }
 }
