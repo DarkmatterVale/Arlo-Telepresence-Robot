dd821b0b7d2f50b49642c51cba8a364c40983617
Vale <valetolpegin@gmail.com>
2014-10-29 09:45:55 -0400

    Added code

--- Arrow-Key-Input/GUIFrame.java
+++ Arrow-Key-Input/GUIFrame.java
@@ -1,1 +1,40 @@
-//Code for frame
+/*
+
+Author: DarkmatterVale ( ValeT )
+
+*/
+
+/*
+
+To Do list:
+  - Complete
+*/
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUIFrame extends JFrame
+{
+  public static void main( String [] args )
+  {
+    //Initialize the JFrame
+    JFrame missionControl = new GUIFrame();
+    
+    //set frame to visible
+    missionControl.setVisible( true );
+  }
+  
+  public void GUIFrame()
+  {
+    //Set frame settings
+    setTitle( "Arlo Telepresence Device Mission Control" );
+    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    setSize( 500, 500 );
+    
+    //Create a GUIPanel object
+    JPanel missionControlPanel = new GUIPanel();
+    
+    //Add panel to frame
+    add( missionControlPanel );
+  }
+}
