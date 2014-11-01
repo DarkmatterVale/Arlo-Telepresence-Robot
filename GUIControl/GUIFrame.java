9f804f87251c60cb31b1fdda0d26ec4e0c5ec8f8
Vale <valetolpegin@gmail.com>
2014-11-01 18:56:18 -0400

    Added Final Version of Code

--- GUIControl/GUIFrame.java
+++ GUIControl/GUIFrame.java
@@ -1,40 +1,33 @@
-/*
+package telepresence.device.test;
 
-Author: DarkmatterVale ( ValeT )
-
-*/
-
-/*
-
-To Do list:
-  - Complete
-*/
-
+/**
+ *
+ * @author vale_000
+ */
 import javax.swing.*;
-import java.awt.*;
 
-public class GUIFrame extends JFrame
+public class TelepresenceDeviceTest extends JFrame
 {
   public static void main( String [] args )
   {
     //Initialize the JFrame
-    JFrame missionControl = new GUIFrame();
-    
-    //set frame to visible
-    missionControl.setVisible( true );
-  }
-  
-  public void GUIFrame()
-  {
-    //Set frame settings
-    setTitle( "Arlo Telepresence Device Mission Control" );
-    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-    setSize( 500, 500 );
+    JFrame missionControl = new TelepresenceDeviceTest();
     
     //Create a GUIPanel object
     JPanel missionControlPanel = new GUIPanel();
     
     //Add panel to frame
-    add( missionControlPanel );
+    missionControl.add( missionControlPanel );
+    
+    //set frame to visible
+    missionControl.setVisible( true );
+  }
+  
+  public void TelepresenceDeviceTest()
+  {
+    //Set frame settings
+    setTitle( "Arlo Telepresence Device Mission Control" );
+    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    setSize( 500, 500 );
   }
 }
