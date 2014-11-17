f25aab42c90cfee1759e82211dbfca876abca0e3
Vale <valetolpegin@gmail.com>
2014-11-17 18:38:40 -0500

    Added Base Code

--- Dynamic-Gmail-Account/GUIFrameRemote.java
+++ Dynamic-Gmail-Account/GUIFrameRemote.java
@@ -1,1 +1,31 @@
-//Add code here
+package telepresence.device.test;
+
+/**
+ *
+ * @author Vale Tolpegin
+ */
+import javax.swing.*;
+
+public class GUIFrameControl extends JFrame
+{
+  public static void main( String [] args )
+  {
+    //Initialize the JFrame
+    JFrame missionControl = new TelepresenceDeviceTest();
+    
+    //Create a GUIPanel object and add panel to frame
+    JPanel missionControlPanel = new GUIPanel();
+    missionControl.add( missionControlPanel );
+    
+    //set frame to visible
+    missionControl.setVisible( true );
+  }
+  
+  public void TelepresenceDeviceTest()
+  {
+    //Set frame settings
+    setTitle( "Arlo Telepresence Device Mission Control" );
+    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    setSize( 500, 500 );
+  }
+}
