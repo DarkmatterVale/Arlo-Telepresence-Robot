ee99aa9f4ad1259ff8fae1888bcdbacf8b9c7a77
Vale <valetolpegin@gmail.com>
2014-11-19 11:47:54 -0500

    Removed comments

--- Dynamic-Gmail-Account/GUIPanelRemote.java
+++ Dynamic-Gmail-Account/GUIPanelRemote.java
@@ -11,16 +11,6 @@ To Do list:
   -Add dynamic Gmail support
 */
 
-/*
-Design:
-For the first version ( v1.0 ) of the GUI, I am thinking of a BorderLayout style with different areas [eventually]
-seperated by borders. The first version shall have 1 non-editable text field that will display what is being sent. In 
-addition, the first version should also include an exit button, start and stop buttons, and an "LED" that will be green if 
-the session/communication is working and red if the session/communication with the robot is not working. Finally, the 1st
-version shall include a Gmail status indicator which will be a JLabel that is in one of two states, OK or NOT WORKING.
-ALL OTHER FEATURES SHOULD BE ADDED TO LATER VERSIONS
-*/
-
 //Importing libraries
 import javax.swing.*;
 import java.awt.*;
